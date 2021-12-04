47a3cfdb3d516d6a0195a1b402669c942fbf2289
sanketcodekul <sanketbhosale.codekul@gmail.com>
2021-12-04 13:39:38 +0530

    HTML FORM

---
+++ intro.js
@@ -0,0 +1,6 @@
+
+      function display() {
+        //document.write("Hello, welcome to codekul");
+       let name = document.getElementById("fname").value
+       document.getElementById("result").innerHTML = name
+      }
