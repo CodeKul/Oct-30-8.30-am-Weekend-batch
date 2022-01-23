c375bdf4d867e8e3e7af54a94a24e3726b0482bb
sanketcodekul <sanketbhosale.codekul@gmail.com>
2022-01-23 09:42:22 +0530

    Javascript Intro

---
+++ javascript/javascript-info.js
@@ -0,0 +1,39 @@
+function getData(){
+    let fullname = document.getElementById("fname").value;
+    console.log(fullname)
+}
+
+
+// let, var and const keywords
+
+// datatypes - String, Number, array, object, boolean
+
+// scope/scoping - global scope, block/local scope function scope
+
+// https://www.youtube.com/watch?v=edlFjlzxkSI
+// https://www.youtube.com/watch?v=DwWtu7VLTwg
+
+// hoisting - https://www.youtube.com/watch?v=Fnlnw8uY6jo
+// hoisting - let, const - https://www.youtube.com/watch?v=BNC6slYCj50
+
+// https://www.youtube.com/watch?v=R0cU7JsNPcY
+
+// https://www.youtube.com/watch?v=9Ta1umc3tkQ
+
+let fname = "Sanket"
+let marks = 100;
+let cities = ["mumbai", "pune", "delhi"]
+
+
+// 1. declaration - declaring there is some variable with respective name
+// 2. Initialization - assigning some value for the variable
+
+
+
+if(true){
+    var num1 =100;
+    console.log(num1)
+}
+
+
+console.log(num1)
